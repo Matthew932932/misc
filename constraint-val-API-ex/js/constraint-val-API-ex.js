7f7777c39359b5ff89bcdab62efccb6bf289faa5
Matthew932932 <184472697+Matthew932932@users.noreply.github.com>
2024-12-10 17:21:37 +1100

    partially done. Was pretty much just the MDN email example.
     Added a user name to try a length check too. I suspect the postcode is to check it's a number and again check min length. And then the password is probably just a string match. The enter country not too sure. The red invalid was in the MDN eaxample so I just transferred that across to user name likewise could be done for ohter fields.

--- constraint-val-API-ex/js/constraint-val-API-ex.js
+++ constraint-val-API-ex/js/constraint-val-API-ex.js
@@ -2,6 +2,11 @@ const form = document.querySelector("form");
 const email = document.getElementById("mail");
 const emailError = document.querySelector("#mail + span.error");
 
+const username = document.getElementById("username");
+const usError = document.querySelector("#username + span.error-us");
+
+
+
 email.addEventListener("input", (event) => {
   if (email.validity.valid) {
     emailError.textContent = ""; // Remove the message content
@@ -11,6 +16,32 @@ email.addEventListener("input", (event) => {
     showError();
   }
 });
+
+username.addEventListener("input", (event) => {
+  if (username.validity.valid) {
+    usError.textContent = ""; // Remove the message content
+    usError.className = "error"; // Removes the `active` class
+  } else {
+    // If there is still an error, show the correct error
+    showUsError();
+  }
+});
+
+function showUsError() {
+  console.log("us1")
+  if (username.validity.valueMissing) {
+    // If empty
+    usError.textContent = "You need to enter a name bro.";
+  } else if (username.validity.typeMismatch) {
+    // If it's not an email address,
+    usError.textContent = "Entered value needs to be an word???.";
+  } else if (username.validity.tooShort) {
+    // If the value is too short,
+    usError.textContent = `Username should be at least ${username.minLength} characters; you entered ${username.value.length}.`;
+  }
+  // Add the `active` class
+  usError.className = "error-us active";
+}
 
 form.addEventListener("submit", (event) => {
   // if the email field is invalid
@@ -36,3 +67,4 @@ function showError() {
   // Add the `active` class
   emailError.className = "error active";
 }
+
